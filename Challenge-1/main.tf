e1faa77c29db42b12496d35f961a9ef748190494
Nitesh-kumar-Reddy <89305638+Nitesh-kumar-Reddy@users.noreply.github.com>
2021-09-08 09:46:50 +0530

    Update main.tf

--- Challenge-1/main.tf
+++ Challenge-1/main.tf
@@ -1,5 +1,11 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" 
+    }
+  }
 }
 
 module "resourcegroup" {
